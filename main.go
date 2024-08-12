da1c1e3364aa5a178b99f599703ec72a57988621
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-12 15:26:13 +0300

    Create post route.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/Jane-Mwangi/GoEventApi/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,14 +11,27 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
-
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "GET /events",
-	})
-	 
+
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+
+}
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created Successfully", " event": event})
 }
--- models/event.go
+++ models/event.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Location    string    `json:"location"`
-	DateTime    time.Time `json:"date_time"`
+	Name        string    `binding:"required" json:"name"`
+	Description string    `binding:"required" json:"description"`
+	Location    string    `"binding:"required" json:"location"`
+	DateTime    time.Time `binding:"required" json:"date_time"`
 	UserID      int       `json:"user_id"`
 
 }
@@ -19,4 +19,9 @@ var events = []Event{
 
 func (e Event) SaveData() {
 	events = append(events, e)
+}
+
+
+func GetAllEvents() []Event {
+	return events
 }
