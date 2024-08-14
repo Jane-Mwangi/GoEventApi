b96725c2c1963bb0165d04780f44bd334920c65c
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-14 20:42:59 +0300

    Register an Update Event Route.

--- routes/events.go
+++ routes/events.go
@@ -13,6 +13,7 @@ func getEvents(context *gin.Context) {
 	events, err := models.GetAllEvents()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events.Try Again Later", "error": err.Error()})
+		return
 	}
 	context.JSON(http.StatusOK, events)
 
@@ -23,14 +24,17 @@ func getEvent(context *gin.Context) {
 	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid Event ID", "error": err.Error()})
+		return
 	}
 	event, err := models.GetEvent(eventId)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event.Try Again Later", "error": err.Error()})
+		return	
 	}
 	context.JSON(http.StatusOK, event)
 
 }
+
 func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
@@ -46,6 +50,29 @@ func createEvent(context *gin.Context) {
 	err = event.Save()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event.Try Again Later", "error": err.Error()})
+		return
 	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Event Created Successfully", " event": event})
+}
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid Event ID", "error": err.Error()})
+		return
+	}
+
+	_,err= models.GetEvent(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event.Try Again Later", "error": err.Error()})
+		return
+	}
+
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data.", "error": err.Error()})
+		return
+	}	
 }
--- routes/routes.go
+++ routes/routes.go
@@ -6,5 +6,6 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
 
 }
