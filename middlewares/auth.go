a25b04a5aaf4643ed9d87d7c5246fd344334221b
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-17 00:22:07 +0300

    Add Authentication Middleware.

---
+++ middlewares/auth.go
@@ -0,0 +1,27 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/Jane-Mwangi/GoEventApi/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized Access"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized Access"})
+		return
+	}
+
+	context.Next()
+
+}
