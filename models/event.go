d5e8fb86ae1cc38c4af176902f8f83c1e4a5d12e
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-18 21:01:29 +0300

    Cancel Registration.

--- models/event.go
+++ models/event.go
@@ -116,3 +116,16 @@ func (e Event) Register(userId int64) error {
 	stmt.Exec(e.ID, userId)
 	return err
 }
+
+func (e Event) CancelRegistration(userId int64) error {
+	query := "DELETE FROM registrations WHERE event_id = ? AND user_id = ?"
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	_, err = stmt.Exec(e.ID, userId)
+	return err 
+}
--- routes/register.go
+++ routes/register.go
@@ -36,5 +36,19 @@ func registerForEvent(context *gin.Context) {
 }
 
 func unregisterForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration for event", "error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Successfully cancelled registration for event"})
 
 }
