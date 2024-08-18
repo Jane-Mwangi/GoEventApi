3de82ba6cced73e0ed7e94e6ec5adadf75ce1f2b
Jane-Mwangi <janiegrace097@gmail.com>
2024-08-18 20:00:57 +0300

    Create Registration Table.

--- db/db.go
+++ db/db.go
@@ -52,4 +52,20 @@ CREATE TABLE IF NOT EXISTS events (
 		panic("Could not create events table: " + err.Error())
 	}
 
+	createRegistrationsTable := `
+CREATE TABLE IF NOT EXISTS registrations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_id INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY(event_id) REFERENCES events(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+	`
+
+	_, err = DB.Exec(createRegistrationsTable)
+
+	if err != nil {
+		panic("Could not create registrations table: " + err.Error())
+	}
+
 }
